dea2e3b42fb853322c406347566f023c5686c719
Mayank Jha <mayankjha8274@gmail.com>
2025-12-02 18:25:43 +0530

    Additional features part1

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,6 +7,27 @@ export default function SearchBox() {
 
     let [city, setCity] = useState("");
 
+    let getWeatherInfo = async () => {
+        try {
+            let response = await fetch(`${import.meta.env.VITE_API_URL}?q=${city}&appid=${import.meta.env.VITE_API_KEY}&units=metric`);
+            if (!response.ok) {
+                throw new Error(`API Error: ${response.status}`);
+            }
+            let jsonResponse = await response.json();
+            let result = {
+                temp : jsonResponse.main.temp,
+                tempMin : jsonResponse.main.temp_min,
+                tempMax : jsonResponse.main.temp_max,
+                humidity : jsonResponse.main.humidity,
+                feelsLike : jsonResponse.main.feels_like,
+                weather : jsonResponse.weather[0].description,
+            };
+            console.log(result);
+        } catch (error) {
+            console.error("Error fetching weather:", error);
+        }
+    }
+
     let handleChange = (evt) => {
         setCity(evt.target.value);
     };
@@ -14,6 +35,7 @@ export default function SearchBox() {
     let handleSubmit = (evt) => {
         evt.preventDefault();
         console.log("Searching weather for:", city);
+        getWeatherInfo();
         setCity("");
     };
 
